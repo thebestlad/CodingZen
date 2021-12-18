0f94fe2714204e9854be440879bb4e3cf09294a8
thebestlad <aaravgupta0910@gmail.com>
2021-12-18 08:04:40 -0600

    12/18/21

---
+++ classwork/test.js
@@ -0,0 +1,16 @@
+var fs = require ("fs")
+var data = "my,name,is,aarav"
+fs.writeFile("learn.csv",data,function(error){})
+
+
+
+
+// for(let i=1; i<=10; i++)
+// {
+//  fs.writeFile(i+".txt","Aarav", function(error){})
+// }
+
+
+
+
+
